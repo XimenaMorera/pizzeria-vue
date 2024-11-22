88db4cc1729bf35ccc3eb8d9d7d5d53f0c2f7d05
Ximena <moreraximena36@gmail.com>
2024-11-21 21:25:45 -0500

    se envian cambios

---
+++ src/components/Ingredient/NewIngredient.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container text-start">
+      <h1 class="text-primary fw-bold">Nuevo Ingrediente</h1>
+      <div class="card">
+        <div class="card-header fw-bold">
+          Crear Ingrediente
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="crearIngrediente">
+            <div class="row mb-3">
+              <label for="name" class="form-label">Nombre:</label>
+              <div class="input-group">
+                <div class="input-group-text">
+                  <font-awesome-icon icon="carrot" />
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  placeholder="Nombre del ingrediente"
+                  v-model="ingrediente.name"
+                  required
+                />
+              </div>
+            </div>
+  
+            <button class="btn btn-primary" type="submit">Crear</button>
+            <button class="btn btn-secondary mx-2" @click="cancelar">Cancelar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  import Swal from "sweetalert2";
+  
+  export default {
+    name: "NewIngredient",
+    data() {
+      return {
+        ingrediente: {
+          name: "",
+        },
+      };
+    },
+    methods: {
+      cancelar() {
+        this.$router.push({ name: "Ingredients" });
+      },
+      async crearIngrediente() {
+        try {
+          const res = await axios.post(
+            "http://127.0.0.1:8000/api/ingredients",
+            this.ingrediente
+          );
+  
+          if (res.status === 201) {
+            this.$router.push({ name: "Ingredients" });
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Ingrediente creado con éxito",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+          }
+        } catch (error) {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "No se pudo crear el ingrediente",
+          });
+        }
+      },
+    },
+  };
+  </script>
+  
